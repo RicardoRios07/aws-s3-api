d558a14a49cd179eed3a7779008663c48c7cabb5
RicardoRios <72361340+RicardoRios07@users.noreply.github.com>
2025-02-05 23:28:43 -0500

    add CORS support to the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');  // Add this line
 const s3Routes = require('./routes/s3Routes');
 const S3Service = require('./services/s3Service');
 
 dotenv.config();
 const app = express();
 const s3Service = new S3Service();
+
+// Enable CORS
+app.use(cors({
+    origin: '*',  // Allow all origins
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],  // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization']  // Allowed headers
+}));
 
 app.use(express.json());
 app.use('/api/s3', s3Routes);
